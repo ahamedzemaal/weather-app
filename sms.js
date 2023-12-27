f10e615a712bf38b5b2807618e66bd435fa268db
ahamedzemaal <zemaalahamed11@gmail.com>
2023-12-27 18:17:40 +0530

    update sms.js

--- sms.js
+++ sms.js
@@ -1,25 +1,35 @@
-const express = require("express");
-const twilio = require("twilio");
-const app = express();
-const dotenv = require("dotenv");
+function sendSMS() {
+  const username = 'ahamedzemaal';
+  const password = 'AhaZem@542k';
+  const apiKey = '520952d142d62deea7c4b9b0ce61c3ff-74865f82-c2ad-48b4-a263-624dcb1694f1';
 
-dotenv.config();
+  const to = +94781736685;//document.getElementById('phone_number').value; // Assuming the phone_number and message come from form inputs
+  const message = document.getElementById('message').value;
 
-async function smsAPI() {
-  const client = new twilio(
-    process.env.TWILIO_SID,
-    process.env.TWILIO_AUTH_TOKEN
-  );
+  const url = 'https://6g9j4z.api.infobip.com/sms/2/text/single';
+  const senderID = 'YourSenderID';
 
-  return client.messages
-    .create({
-      body: "Hi" ,
-      from: "+12052361069",
-      to: process.env.PHONE_NUMBER,
-    })
-    .then((message) => console.log(message, "Message Send"))
-    .catch((error) => console.log("Error: ", error));
+  const headers = new Headers();
+  headers.append('Authorization', 'Basic ' + btoa(`${username}:${password}`));
+  headers.append('Content-Type', 'application/json');
+  headers.append('x-api-key', apiKey);
+
+  const data = {
+      from: senderID,
+      to: to,
+      text: message
+  };
+
+  fetch(url, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(data)
+  })
+      .then(response => response.json())
+      .then(result => {
+          console.log('Message sent successfully!', result);
+      })
+      .catch(error => {
+          console.error('Error:', error);
+      });
 }
-
-smsAPI();
-app.listen(5000, () => console.log("Listening at port 5000"));
